937b79e6dc9ab7b1b700f555d5c469cdeb10b65f
Lisa MacCarrigan and Meredith Alcorn <lisa+meredith@>
2016-12-13 12:03:15 -0500

    add custom module for ajax-enabled news article view

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'news_feed',
+      'username' => 'admin',
+      'password' => 'admin',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'K0BCrXNjUxZN4BtD0hBbJ5f9CfoTk8iOmNZBSR4sIc0';
 
 /**
  * Base URL (optional).
